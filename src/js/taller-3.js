bdb944c07b0b8d454311901045b6472bcea8aa8a
andiparo <andiparodi.20@gmail.com>
2022-02-14 17:17:11 +0100

    Finalizando taller 3

--- src/js/taller-3.js
+++ src/js/taller-3.js
@@ -1,3 +1,90 @@
+// ----------------------> Lista
+
+function switchFormula () {
+  const f = document.getElementById('formulaSelect').value;
+  const btn = document.getElementById('btn-render');
+  cleanError('form');
+  switch (f) {
+      case '1':
+          btn.setAttribute('onclick', 'calcularPromedio()');
+          btn.innerText = 'Calcular Promedio'
+          break;
+      case '2':
+          btn.setAttribute('onclick', 'calcArmonica()');
+          btn.innerText = 'Calcular Media Armonica'
+          break;
+      case '3':
+          btn.setAttribute('onclick', 'calcularGeometrica();');
+          btn.innerText = 'Calcular Media Geometrica'
+          break;
+      case '4':
+          btn.setAttribute('onclick', 'calculoModa()');
+          btn.innerText = 'Calcular Moda'
+          break;
+      case '5':
+          btn.setAttribute('onclick', 'calcMediana();');
+          btn.innerText = 'Calcular Mediana'
+          break;
+  }
+}
+
+let nuevaLista = []
+
+const cleanError = (id) => {
+  const errors = document.getElementById(id).querySelectorAll('.error');
+  for (e of errors) {
+      e.innerHTML = '';
+  }
+}
+
+const renderList = (list) => {
+  l = document.getElementById('userLista');
+  l.innerHTML = '';
+
+  list.forEach((element, index) => {
+      const listElement = document.createElement('li');
+      listElement.innerHTML = `<strong>${index + 1} -</strong> ${element}`;
+
+      l.appendChild(listElement);
+  });
+}
+
+const agregarValorLista = (event) => {
+  cleanError('form')
+  event.preventDefault()
+
+  const userList = document.getElementById("valorLista")
+  const value = Number(userList.value)
+  userList.value = ''
+  
+  if (Number.isNaN(value) || value < 0) {
+      e = document.getElementById('errorAddValue');
+      e.innerHTML = 'Ingrese un valor válido para la lista de números';
+      return false;
+  }
+
+  nuevaLista.push(value)
+  renderList(nuevaLista)
+}
+
+const quitarValor = () => {
+    cleanError('form')
+    const element = document.getElementById('removeValue')
+    let value = parseInt(element.value);
+    element.value = ''
+
+    if (Number.isNaN(value) || value <= 0) {
+        e = document.getElementById('errorRemoveValue')
+        e.innerHTML = 'Ingrese una posición válida de la lista de números'
+        return false
+    }
+
+    value -= 1
+
+    nuevaLista.splice(value, 1)
+    renderList(nuevaLista)
+}
+
 // ----------------------> Promedio
 
 let sumaLista = 0
@@ -11,7 +98,7 @@ const calcularPromedio = (lista) => {
   return promedioLista
 }
 
-let nuevaLista = []
+
 
 const calculoPromedio = () => {
 
@@ -21,185 +108,98 @@ const calculoPromedio = () => {
 
   const result = calcularPromedio(nuevaLista)
   
-  userLista.innerText = `El promedio es de ${result}`
+  resultPromedio.innerText = `El promedio es de ${result}`
 }
 
-const agregarValorLista = (event) => {
-  event.preventDefault()
-
-  const userList = document.getElementById("valorLista")
-  const value = Number(userList.value)
-  userList.value = ''
-  
-  nuevaLista.push(value)
-  userLista.innerText = `${nuevaLista}`;
-  console.log(nuevaLista)
-}
 
 // ----------------------> Mediana
-// const calcularPromedio = (lista) => {
 
-//   const sumaLista = lista.reduce((valorAcumulado = 0, nuevoElemento) =>
-//      valorAcumulado + nuevoElemento)
+const esPar = (n) => n % 2 === 0
 
-//   const promedioLista = sumaLista / lista.length
-//   return promedioLista
-// }
+const ordernarLista = (lista) => {
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+  lista.sort(compareNumbers)
+}
 
-// let nuevaLista = []
+let mediana;
 
-// const esPar = (n) => n % 2 === 0
+const calcMediana = () => {
 
+  ordernarLista(nuevaLista)
 
-// const ordernarLista = (lista) => {
-//   function compareNumbers(a, b) {
-//     return a - b;
-//   }
-//   lista.sort(compareNumbers)
-// }
+  const mitadLista = parseInt(nuevaLista.length / 2)
 
-// const agregarValorLista = (event) => {
-//   event.preventDefault()
+  if(esPar(nuevaLista.length)) {
+    const elemento1 = nuevaLista[mitadLista - 1]
+    const elemento2 = nuevaLista[mitadLista] 
 
-//   const userList = document.getElementById("valorLista")
-//   const value = Number(userList.value)
-//   userList.value = ''
-  
-//   nuevaLista.push(value)
-
-//   ordernarLista(nuevaLista)
-
-//   userLista.innerText = `${nuevaLista}`;
-//   console.log(nuevaLista)
-// }
-
-// let mediana;
-
-// const calcMediana = () => {
-
-//   const mitadLista = parseInt(nuevaLista.length / 2)
-
-//   if(esPar(nuevaLista.length)) {
-//     const elemento1 = nuevaLista[mitadLista - 1]
-//     const elemento2 = nuevaLista[mitadLista] 
-
-//     const result = calcularPromedio([elemento1, elemento2])
-//     console.log(result)
+    const mediana = calcularPromedio([elemento1, elemento2])
+    console.log(mediana)
     
+    resultMediana.innerText = `La mediana es de ${mediana}`
 
-// } else {
-//     mediana = nuevaLista[mitadLista]
-//     console.log(mediana)
-//   }
-// }
+  } else {
+    const mediana = nuevaLista[mitadLista]
+    resultMediana.innerText = `La mediana es de ${mediana}`    
+  }
+}
 
 // // ----------------------> Moda
 
-// let nuevaLista = []
+const calculoModa = () => {
+  const listaCount = {};
 
-// const agregarValorLista = (event) => {
-//   event.preventDefault()
+  const cantidadNumeros = (n) => {
+    if (listaCount[n]) {  
+      listaCount[n] += 1
+    } else {
+      listaCount[n] = 1
+    }
+  }
 
-//   const userList = document.getElementById("valorLista")
-//   const value = Number(userList.value)
-//   userList.value = ''
-  
-//   nuevaLista.push(value)
+  nuevaLista.map(elemento => {
+    cantidadNumeros(elemento)
+  });
 
-//   userLista.innerText = `${nuevaLista}`;
-//   console.log(nuevaLista)
-// }
+  const listaArray = Object.entries(listaCount).sort((elementoA, elementoB) => elementoA[1] - elementoB[1])
 
-// const calculoModa = () => {
-//   const listaCount = {};
+  const moda = listaArray[listaArray.length - 1]
 
-//   const cantidadNumeros = (n) => {
-//     if (listaCount[n]) {  
-//       listaCount[n] += 1
-//     } else {
-//       listaCount[n] = 1
-//     }
-//   }
-
-//   nuevaLista.map(elemento => {
-//     cantidadNumeros(elemento)
-//   });
-
-//   const listaArray = Object.entries(listaCount).sort((elementoA, elementoB) => elementoA[1] - elementoB[1])
-
-//   const moda = listaArray[listaArray.length - 1]
-
-//   console.log(moda)
-
-//   userLista.innerText = `la moda de esta lista es de ${moda[0]} y se repite ${moda[1]} veces`;
-
-// }
+  resultModa.innerText = `La moda en esta lista es de ${moda[0]}`;
+}
 
 
-// // ----------------------> Armonica
+//----------------------> Armonica
+const calcArmonica = (lista) => {
+  const sumaList = lista.reduce(
+    (valorAcumulado, nuevoElemento) => valorAcumulado + (1 / nuevoElemento), 0
+);
 
-// let nuevaLista = []
+const mediaArmonica = lista.length / sumaList;
+return parseFloat(mediaArmonica.toFixed(3));
+} 
 
-// const agregarValorLista = (event) => {
-//   event.preventDefault()
-
-//   const userList = document.getElementById("valorLista")
-//   const value = Number(userList.value)
-//   userList.value = ''
-  
-//   nuevaLista.push(value)
-
-//   userLista.innerText = `${nuevaLista}`;
-//   console.log(nuevaLista)
-// }
-
-// const calcArmonica = (lista) => {
-//   const sumaList = lista.reduce(
-//     (valorAcumulado, nuevoElemento) => valorAcumulado + (1 / nuevoElemento), 0
-// );
-
-// const mediaArmonica = lista.length / sumaList;
-// return parseFloat(mediaArmonica.toFixed(3));
-// } 
-
-// const calculoArmonica = () => {
-//   const result = calcArmonica(nuevaLista)
-
-
-//   userLista.innerText = `la armónica de la lista es de ${result}`;
-// }
+const calculoArmonica = () => {
+  const result = calcArmonica(nuevaLista)
+  resultArmonica.innerText = `La armónica de la lista es de ${result}`;
+}
 
 // // ----------------------> Geometrica
 
-// let nuevaLista = []
+const calcularGeometrica = () => {
+  const productoElementos = nuevaLista.reduce(
+    (elementoAcumulado, elementoIterado) => { 
+    return elementoAcumulado * elementoIterado
+    }, 1
+)
+   const mediaGeometrica = productoElementos ** (1/nuevaLista.length);
+   return parseFloat(mediaGeometrica.toFixed(3));
+}
 
-// const agregarValorLista = (event) => {
-//   event.preventDefault()
+const calculoGeometrica = () => {
+  const result = calcularGeometrica(nuevaLista)
 
-//   const userList = document.getElementById("valorLista")
-//   const value = Number(userList.value)
-//   userList.value = ''
-  
-//   nuevaLista.push(value)
-
-//   userLista.innerText = `${nuevaLista}`;
-//   console.log(nuevaLista)
-// }
-
-
-// const calcularGeometrica = () => {
-//   const productoElementos = nuevaLista.reduce(
-//     (elementoAcumulado, elementoIterado) => { 
-//     return elementoAcumulado * elementoIterado
-//     }, 1
-// )
-//    const mediaGeometrica = productoElementos ** (1/nuevaLista.length);
-//    return parseFloat(mediaGeometrica.toFixed(3));
-// }
-
-// const calculoGeometrica = () => {
-//   const result = calcularGeometrica(nuevaLista)
-
-
-//   userLista.innerText = `la geométrica de la lista es de ${result}`;
-// }
+  resultGeometrica.innerText = `la geométrica de la lista es de ${result}`;
+}
